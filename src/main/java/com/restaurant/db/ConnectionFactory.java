bba8f9eaf7004ef8d39ee58c8e31ccca4f4110c5
Chatterjee <abhrak.chatterjee@capgemini.com>
2018-08-27 17:50:50 +0530

    First Commit

---
+++ src/main/java/com/restaurant/db/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package com.restaurant.db;
+
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
+
+public class ConnectionFactory
+{
+	static MongoClient mongo;
+	static DB db;
+	public static MongoDatabase getDB()
+	{
+		  mongo = new MongoClient( "localhost" , 27017 ); 
+	      // Accessing the database 
+	      MongoDatabase database = mongo.getDatabase("RestaurantDB");  
+	      return database;
+	} 
+	@SuppressWarnings({ "deprecation" })
+	public static DB getDB1()
+	{    
+		mongo = new MongoClient( "localhost" , 27017 );
+		db = mongo.getDB("RestaurantDB");  
+	    return db;
+	}
+}
---
+++ src/main/java/com/restaurant/main/RestaurantTest.java
@@ -0,0 +1,14 @@
+package com.restaurant.main;
+
+import com.restaurant.query.RestaurantDetails;
+
+public class RestaurantTest 
+{
+	
+	public static void main(String[] args)
+	{
+		RestaurantDetails resDet=new RestaurantDetails();
+//		resDet.getAllRestaurants();
+		resDet.query2();
+	}
+}
